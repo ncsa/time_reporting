18593c9cd3dadc77617d8063384930dba9750048
EdTheDev <edthedev@gmail.com>
2012-10-17 12:53:42 -0500

    Made the methods more reusable and then tightened up the main function a bit.

--- time_reporting/time_reporting.py
+++ time_reporting/time_reporting.py
@@ -39,7 +39,14 @@ def login():
     result = session.post(LOGIN_URL, data={'inputEnterpriseId': USERNAME, 'password': pwd, 'queryString': 'null', 'BTN_LOGIN': 'Login'}, allow_redirects=True)
     return result.content
 
-def submit(hours):
+def submit(date_string=None, hours=[0,8,8,8,8,8,0]):
+
+    # Go to the requested date.
+    if date_string is not None:
+        url = get_url_for_date(date_string)
+        print url
+        result = session.get(url).content
+
     if len(hours) != 7:
         raise ValueError("Expected 7 values for Sunday-Saturday")
     d = {}
@@ -58,41 +65,51 @@ def submit(hours):
     result = session.post(SUBMIT_URL, data=d, allow_redirects=True)
     return "You have successfully submitted" in result.content
 
+def get_url_for_date(date_string):
+    month, day, year = [int(x) for x in date_string.split('/')]
+    date = datetime.date(year, month, day)
+    url = OVERDUE_URL + "month=" + str(date.month) + "&selectedWeek=" + date_string
+    return url
+    
 def main():
+    # Current date.
+    date_string = None
+    # Typical work week.
+    hours = [0, 8, 8, 8, 8, 8, 0]
+
+    result = ''
+
     if not isLoggedIn():
         result = login()
-        if len(sys.argv) == 2 or len(sys.argv) == 9:
-            month, day, year = [int(x) for x in sys.argv[1].split('/')]
-            date = datetime.date(year, month, day)
-            url = OVERDUE_URL + "month=" + str(date.month) + "&selectedWeek=" + sys.argv[1]
-            result = session.get(url).content
+    
     if "Edit" not in result:
         if len(sys.argv) == 1 or len(sys.argv) == 2:
-            if submit([0, 8, 8, 8, 8, 8, 0]):
-                print "Successfully submitted."
-                result = session.get(url).content
+            date_string = sys.argv[1]
         elif len(sys.argv) == 8 or len(sys.argv) == 9:
             try:
-                l = [float(x) for x in sys.argv[-7:]]
+                hours = [float(x) for x in sys.argv[-7:]]
             except ValueError:
                 usage()
                 raise
                 sys.exit(1)
-            if submit(l):
-                print "Successfully submitted."
-                result = session.get(url).content
+        if submit(date_string, hours):
+            print "Successfully submitted."
+            result = session.get(url).content
         else:
             print "Error: Only", len(sys.argv), "arguments found."
             usage()
             sys.exit(1)
     else:
         print "Time reporting for this week is up to date."
+
     if "Submission of time for the following week(s) is overdue." in result:
-        print "Warning: Overdue time reports."
         overdue = [x.strip() for x in result[result.find('id="pastDueWeek">'):result.find('</select>&nbsp;<input type="submit" id="getPastDueTimeEntryForm"')].split('\n') if x.strip()][1:]
         overdue = [x[x.find('month='):x.find('">')] for x in overdue]
         overdue = [x[x.find('Week=')+5:] for x in overdue]
-        print '\n'.join(overdue)
+
+        print "Warning: Overdue time reports (" + str(len(overdue)) + ")."
+        if len(overdue) < 10:
+            print '\n'.join(overdue)
 
 if __name__ == "__main__":
     main()
