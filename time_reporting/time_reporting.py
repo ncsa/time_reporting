38599678d2a0a739b8dc85bdb43530b29d751c8a
EdTheDev <edthedev@gmail.com>
2012-10-31 13:43:27 -0500

    Fixed some buggy behavior in edge cases. Added a loop that prompts if you have overdue time to submit.

--- time_reporting/time_reporting.py
+++ time_reporting/time_reporting.py
@@ -3,12 +3,13 @@
 import requests
 import getpass
 import sys
-import datetime
+from datetime import datetime, timedelta, date
 
 URL         = "https://hrnet.uihr.uillinois.edu/PTRApplication/index.cfm"
 LOGIN_URL   = "https://eas.admin.uillinois.edu/eas/servlet/login.do"
 OVERDUE_URL = URL + "?fuseaction=TimesheetEntryForm&Overdue=true&"
 SUBMIT_URL  = URL + "?fuseaction=SubmitTimesheet" 
+DATE_FORMAT = '%m/%d/%Y'
 
 USERNAME = getpass.getuser()
 session = requests.session()
@@ -25,7 +26,6 @@ def usage():
                date  - date for overdue time reporting
                        Example: 01/21/1999"""
 
-
 def isLoggedIn():
     result = session.get(URL).content
     if "easFormId" in result:
@@ -39,13 +39,35 @@ def login():
     result = session.post(LOGIN_URL, data={'inputEnterpriseId': USERNAME, 'password': pwd, 'queryString': 'null', 'BTN_LOGIN': 'Login'}, allow_redirects=True)
     return result.content
 
-def submit(date_string=None, hours=[0,8,8,8,8,8,0]):
+def get_hours_from_string(hours_string):
+    return validate_hours(hours_string.split(' '))
+
+def validate_hours(hours):
+    try:
+        hours = [float(x) for x in sys.argv[-7:]]
+        return hours
+    except ValueError:
+        print "Invalid hours provided. Must be floats."
+        usage()
+        return None 
+
+def submit(date_string=None, hours=None, silent = False):
 
     # Go to the requested date.
-    if date_string is not None:
-        url = get_url_for_date(date_string)
-        print url
-        result = session.get(url).content
+    if date_string is None:
+        date_string = get_recent_sunday()
+    
+# Prompt to be sure entered value is correct.
+    if not silent:
+        choice = raw_input('Submit ' + str(hours) + ' for ' + date_string + \
+                '? [Y/n]')
+        yep = ['', 'Y', 'y', 'yes', 'Yes']
+        if not choice in yep:
+            return "Cancelled by user." 
+
+    url = get_url_for_date(date_string)
+    # print url
+    result = session.get(url).content
 
     if len(hours) != 7:
         raise ValueError("Expected 7 values for Sunday-Saturday")
@@ -63,45 +85,49 @@ def submit(date_string=None, hours=[0,8,8,8,8,8,0]):
     d['weekTotalHours'] = int(total/60)
     d['weekTotalMinutes'] = total % 60
     result = session.post(SUBMIT_URL, data=d, allow_redirects=True)
-    return "You have successfully submitted" in result.content
+    if "You have successfully submitted" in result.content:
+        return "Successfully submitted %s for %s." % (str(hours), date_string)
+    else:
+        return "Unable to submit %s for %s. " % (str(hours), date_string) + \
+                "Have you already submitted this date?" 
 
 def get_url_for_date(date_string):
     month, day, year = [int(x) for x in date_string.split('/')]
-    date = datetime.date(year, month, day)
-    url = OVERDUE_URL + "month=" + str(date.month) + "&selectedWeek=" + date_string
+    the_date = date(year, month, day)
+    url = OVERDUE_URL + "month=" + str(the_date.month) + "&selectedWeek=" + date_string
     return url
-    
+
+def get_recent_sunday():
+    day = datetime.now() 
+    while day.strftime('%A') != 'Sunday':
+        day = day - timedelta(days=1)
+    return day.strftime(DATE_FORMAT)
+
 def main():
-    # Current date.
     date_string = None
-    # Typical work week.
-    hours = [0, 8, 8, 8, 8, 8, 0]
-
+    hours = None
     result = ''
+# Parse command line arguments.
+    if len(sys.argv) == 2:
+        date_string = sys.argv[1]
+    elif len(sys.argv) == 8 or len(sys.argv) == 9:
+        hours = validate_hours(sys.argv[-7:])
+        
+    if hours is None:
+        usage()
+        sys.exit()
 
+# Login
     if not isLoggedIn():
         result = login()
-    
-    if "Edit" not in result:
-        if len(sys.argv) == 1 or len(sys.argv) == 2:
-            date_string = sys.argv[1]
-        elif len(sys.argv) == 8 or len(sys.argv) == 9:
-            try:
-                hours = [float(x) for x in sys.argv[-7:]]
-            except ValueError:
-                usage()
-                raise
-                sys.exit(1)
-        if submit(date_string, hours):
-            print "Successfully submitted."
-            result = session.get(url).content
-        else:
-            print "Error: Only", len(sys.argv), "arguments found."
-            usage()
-            sys.exit(1)
-    else:
+   
+# Submit time
+    if "Edit" in result:
         print "Time reporting for this week is up to date."
+    else:
+        print submit(date_string, hours)
 
+        # Review overdue time
     if "Submission of time for the following week(s) is overdue." in result:
         overdue = [x.strip() for x in result[result.find('id="pastDueWeek">'):result.find('</select>&nbsp;<input type="submit" id="getPastDueTimeEntryForm"')].split('\n') if x.strip()][1:]
         overdue = [x[x.find('month='):x.find('">')] for x in overdue]
@@ -110,6 +136,9 @@ def main():
         print "Warning: Overdue time reports (" + str(len(overdue)) + ")."
         if len(overdue) < 10:
             print '\n'.join(overdue)
+
+        for day in overdue:
+            print submit(day, hours)
 
 if __name__ == "__main__":
     main()
