b2c0fc9093b4b046597897149800a416748a9db9
Andy Loftus <aloftus@gmail.com>
2018-04-06 07:39:21 -0500

    query latest docker image tag at runtime

--- docker_run.sh
+++ docker_run.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
+###
+# BEGIN CUSTOMIZATIONS
+###
+
 # Uncomment to turn on debugging
 DEBUG=1
 
 # Uncomment to enable test mode (show what would be run)
 TEST=1
 
-# Set Image Name
-IMAGE="andylytical/ncsa-time-reporting:20171029"
+# Set Docker Image Parts
+# (don't need docker image tag, it will be queried at runtime)
+DK_USER=andylytical
+DK_IMAGE=ncsa-time-reporting
 
 # Set volume mount (to provide .netrc file)
 # Format is 2-tuple of (local source, mount point inside container)
-MOUNTPOINT=( "j:\\aloftus\\private" "/private" )
+MOUNTPOINT=( "j:\\aloftus\\private" "/private" ) # WINDOWS
+#MOUNTPOINT=( "$HOME" "/private" ) # LINUX
 
 # Set Environment Variable(s) for Container
 # Possible environment vars are:
 #   NETRC                ( default: ~/.netrc )
-#   PYEXCH_REGEX_JSON    ( default: pyexch.PyExch.DEFAULT_REGEX_MAP )
+#   PYEXCH_REGEX_JSON    ( No Default, This Is Required )
 NETRC=/private/.netrc
 #PYEXCH_REGEX_JSON=
 
@@ -24,10 +31,35 @@ NETRC=/private/.netrc
 # END OF CUSTOMIZATIONS
 ###
 
+function die() {
+    echo "ERR: $*"
+    exit 99
+}
+
+function latest_docker_tag() {
+    # Based on code from:
+    # https://stackoverflow.com/questions/28320134/how-to-list-all-tags-for-a-docker-image-on-a-remote-registry
+    [[ $DEBUG -eq 1 ]] && set -x
+    [[ $# -ne 1 ]] && die "latest_docker_tag: must have exactly 1 parameter, got '$#'"
+    image="$1"
+    wget -q https://registry.hub.docker.com/v1/repositories/${image}/tags -O - \
+    | sed -e 's/[][]//g' \
+          -e 's/"//g' \
+          -e 's/ //g' \
+    | tr '}' '\n' \
+    | awk -F: '{print $3}' \
+    | sort -r \
+    | head -1
+}
+
 [[ $DEBUG -eq 1 ]] && set -x
 
 action=
 [[ $TEST -eq 1 ]] && action=echo
+
+DK_TAG=$( latest_docker_tag "$DK_USER/$DK_IMAGE" )
+[[ -z "$DK_TAG" ]] && die "No tags found for docker image: '$DK_USER/$DK_IMAGE'"
+IMAGE="$DK_USER/$DK_IMAGE:$DK_TAG"
 
 envs=()
 for v in NETRC ${!PYEXCH_*} ; do
